c127b8f6d7e67e24e5a6628bcc23664b75ad9816
kaeka <Sulis347@gmail.com>
2023-08-15 01:01:59 +0900

    add-own-impl-emd

--- replicate_EMDMI_MODWT.m
+++ replicate_EMDMI_MODWT.m
@@ -17,7 +17,7 @@ bg = reshape(space3D(:,bgx,bgy),1024,size(bgx,2)*size(bgy,2));
 SNRinit = snr(sg,bg);
 
 % add additive gaussian white noise
-dataSNR = 60; % in dB, set a stupidly high SNR to make them okay
+dataSNR = 70; % in dB, set a stupidly high SNR to make them okay
 space3Dnoised = awgn(space3D,dataSNR);
 sg = reshape(space3Dnoised(:,sgx,sgy),1024,size(sgx,2)*size(sgy,2));
 bg = reshape(space3Dnoised(:,bgx,bgy),1024,size(bgx,2)*size(bgy,2));
@@ -49,8 +49,51 @@ organized_show_image(denoisedPaper1Wavelet);
 organized_show_image(denoisedPaper2DWT);
 organized_show_image(denoisedPaper2MODWT);
 %% Do EMD on one of their signal
-% signal = space3D(:,101,32);
-% emd(signal);
-% [imfs,residual] = emd(signal);
-% figure;plot(imfs)
+D = space3Dbpf;
+imsize = size(D,2);
+dat2d = reshape(D,1024,imsize^2);
+denoised = zeros(size(dat2d));
+tic
+parfor sgsel = 1:size(dat2d,2)
+    signal = dat2d(:,sgsel);
+    [imfs,residual] = emd(signal);
+    
+    partition = ceil(size(imfs,2)/2);
+    tail = size(imfs,2);
+    % find the partition by MI
+    cond = true;
+    while cond
+        mi_with_noisy_signal = mi(signal,imfs(:,partition));
+        selected_group = partition+1:tail;
+        mi_with_selected_set = mi(imfs(:,partition),sum(imfs(:,selected_group),2));
+        score = mi_with_noisy_signal - mi_with_selected_set;
 
+        if(score < 0)% closer to group than noisy
+            partition = partition -1;
+        else
+            cond = false;
+        end
+
+        if partition == 0
+            cond = false;
+        end
+    end
+    
+
+    high_f_grp = 1:partition;
+    low_f_grp = partition+1:tail;
+    rec = sum(imfs(:,low_f_grp),2);
+    % apply thresholding to high_f_grp
+    for i =1:numel(high_f_grp)
+        imf_t = imfs(:,high_f_grp(i));
+        rec = rec + wthresh(imf_t,'h',thselect(imf_t,'rigrsure'));
+    end
+    % for the thresholding: https://www.mathworks.com/help/wavelet/ref/thselect.html
+
+    denoised(:,sgsel) = rec;
+
+end
+toc
+%%
+rec3d= reshape(denoised,1024,imsize,imsize);
+organized_show_image(rec3d)
